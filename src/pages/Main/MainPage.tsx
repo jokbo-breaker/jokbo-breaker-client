2be2f3df6814271dfee5527c46743026a9c0bf68
Dubabbi <thdms3160@naver.com>
2025-08-16 23:56:26 +0900

    style: 디자인 시스템에 Typography 적용 (#2)

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -3,7 +3,7 @@ import Icon from '@/shared/components/common/icon';
 export default function MainPage() {
   return (
     <div className="flex gap-4 p-4">
-      <h1 className="text-primary-900 text-title-lg">Main</h1>
+      <h1 className="text-head1">헤드1 22/SB</h1>
       <Icon name="home" size={2.4} className="text-accent-500" />
     </div>
   );
