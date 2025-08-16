5602c4ed2deae9e8f170e22def01b5f48cc3288d
Dubabbi <thdms3160@naver.com>
2025-08-17 00:05:59 +0900

    style: 디자인 시스템 Color 적용 (#2)

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -2,8 +2,9 @@ import Icon from '@/shared/components/common/icon';
 
 export default function MainPage() {
   return (
-    <div className="flex gap-4 p-4">
-      <h1 className="text-head1">헤드1 22/SB</h1>
+    <div className="flex-col gap-4 p-4">
+      <h1 className="text-head1 text-primary">헤드1 22/SB</h1>
+      <h2 className="text-head2 text-secondary">헤드2 20/SB</h2>
       <Icon name="home" size={2.4} className="text-accent-500" />
     </div>
   );
