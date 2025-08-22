97fbe8b43587dbb34f0244cec4616c5636b5f54f
Dubabbi <thdms3160@naver.com>
2025-08-22 15:19:50 +0900

    feat: 추천 결과 empty 컴포넌트 구현 (#57)

---
+++ src/pages/recommend/components/empty-recommend.tsx
@@ -0,0 +1,13 @@
+import Icon from '@/shared/components/icon'; // Icon name="empty-recommend" size={25}
+
+export default function EmptyRecommend() {
+  return (
+    <div className="flex-col-center gap-[2.8rem]">
+      <Icon name="empty-recommend" size={25} />
+      <div className="flex-col-center gap-[0.8rem] text-black">
+        <span className="head3">취향에 맞는 준비된 메뉴가 없어요</span>
+        <span className="body2">다시 추천받아 볼까요?</span>
+      </div>
+    </div>
+  );
+}
